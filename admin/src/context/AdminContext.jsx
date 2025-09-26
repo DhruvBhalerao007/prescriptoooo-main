37235982070cfb9f106d68f6cef66e4218c04bfd
DhruvBhalerao007 <dhruvbhalerao007@gmail.com>
2025-09-26 18:32:58 +0530

    Update AdminContext.jsx

--- admin/src/context/AdminContext.jsx
+++ admin/src/context/AdminContext.jsx
@@ -7,8 +7,7 @@ export const AdminContext = createContext()
 
 const AdminContextProvider = (props) => {
 
-    const backendUrl = "https://prescriptoooo-main-backend.onrender.com"
-        // import.meta.env.VITE_BACKEND_URL
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
 
     const [aToken, setAToken] = useState(localStorage.getItem('aToken') ? localStorage.getItem('aToken') : '')
 
